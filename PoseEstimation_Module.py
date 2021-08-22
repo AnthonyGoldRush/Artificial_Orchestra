33405142887726efbb5ab3f9d18713438e2eaa89
AnthonyGoldRush <48116857+AnthonyGoldRush@users.noreply.github.com>
2021-08-21 23:47:11 -0700

    Update PoseEstimation_Module.py

--- PoseEstimation_Module.py
+++ PoseEstimation_Module.py
@@ -2,37 +2,48 @@ import cv2
 import mediapipe as mp
 import time
 
-class poseDetector():
+class PoseDetector:
 
-    def __init__(self, mode = False, upBody = False, smooth = True,
-                 detectionCon = 0.5, trackCon = 0.5):
+    def __init__(self, mode = False, upBody = False, smooth=True, detectionCon = 0.5, trackCon = 0.5):
 
         self.mode = mode
         self.upBody = upBody
-        self.smooth =  smooth
+        self.smooth = smooth
         self.detectionCon = detectionCon
         self.trackCon = trackCon
 
+        static_image_mode = False,
+        model_complexity = 1,
+        smooth_landmarks = True,
+        min_detection_confidence = 0.5,
+        min_tracking_confidence = 0.5):
 
         self.mpDraw = mp.solutions.drawing_utils
         self.mpPose = mp.solutions.pose
-        self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth,
-                                     self.detectionCon, self.trackCon)
+        self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth, self.detectionCon, self.trackCon)
 
-    def findPose(self, img, draw= True):
+    def findPose(self, img, draw=True):
+
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = self.pose.process(imgRGB)
-        if results.pose_landmarks:
+        self.results = self.pose.process(imgRGB)
+        #print(results.pose_landmarks)
+        if self.results.pose_landmarks:
             if draw:
-                self.mpDraw.draw_landmarks(img, results.pose_landmarks,
-                                            self.mpPose.POSE_CONNECTIONS)
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+
         return img
 
-      # for id, lm in enumerate(results.pose_landmarks.landmark):
-      #      h, w, c = img.shape
-      # print(id, lm)
-      # cx, cy = int(lm.x*w), int(lm.y*h)
-      # cv2.circle(img,(cx,cy), 5, (255,0,0), cv2.FILLED)
+    def getPosition(self, img, draw=True):
+        lmList = []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                 h, w, c = img.shape
+        # print(id, lm)
+        cx, cy = int(lm.x * w), int(lm.y * h)
+        lmList.append([id, cx, cy])
+        if draw:
+            cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+        return lmList
 
 
 
@@ -40,17 +51,18 @@ class poseDetector():
 def main():
     cap = cv2.VideoCapture('Video_1/Arms.mp4')
     pTime = 0
-    detector = poseDetector()
+    detector = PoseDetector()
     while True:
         success, img = cap.read()
         img = detector.findPose(img)
+        lmList = detector.getPosition(img)
+        print(lmList)
 
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
 
-        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
-                    (255, 0, 0), 3)
+        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
 
         cv2.imshow("Image", img)
         cv2.waitKey(1)
