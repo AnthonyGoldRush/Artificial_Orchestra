4206cd7b4e8748cc9f03c99b40449187e4c9c4e8
AnthonyGoldRush <48116857+AnthonyGoldRush@users.noreply.github.com>
2021-08-25 01:12:55 -0700

    Update FaceDetection_Module.py

--- FaceDetection_Module.py
+++ FaceDetection_Module.py
@@ -25,12 +25,37 @@ class FaceDetector():
                 bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
                        int(bboxC.width * iw), int(bboxC.height * ih)
                 bboxs.append([id, bbox, detection.score])
+                if draw:
+                    img = self.fancyDraw(img, bbox)
 
-                cv2.rectangle(img,bbox, (255, 0, 255), 2)
-                cv2.putText(img, f'{int(detection.score[0]*100)}%', (bbox[0], bbox[1]-20),
-                            cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
+                    cv2.putText(img, f'{int(detection.score[0]*100)}%', (bbox[0], bbox[1]-20),
+                                cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
 
         return img, bboxs
+
+    def fancyDraw(self, img, bbox, l = 30, t =7, rt = 1):
+        x, y, w, h = bbox
+        x1, y1 = x+w, y + h
+
+        cv2.rectangle(img, bbox, (255, 0, 255), rt)
+        #Top L
+        cv2.line(img, (x,y), (x+l,y),(255,0,255), t)
+        cv2.line(img, (x, y), (x, y+l), (255, 0, 255), t)
+
+        # Top R
+        cv2.line(img, (x1, y), (x1 - l, y), (255, 0, 255), t)
+        cv2.line(img, (x1, y), (x1, y + l), (255, 0, 255), t)
+
+        # Bottom L
+        cv2.line(img, (x, y1), (x + l, y1), (255, 0, 255), t)
+        cv2.line(img, (x, y1), (x, y1 - l), (255, 0, 255), t)
+
+        # Bottom R
+        cv2.line(img, (x1, y1), (x1 - l, y1), (255, 0, 255), t)
+        cv2.line(img, (x1, y1), (x1, y1 - l), (255, 0, 255), t)
+
+        return img
+
 
 
 
@@ -42,6 +67,8 @@ def main():
     while True:
         success, img = cap.read()
         img, bboxs = detector.findFaces(img)
+        print(bboxs)
+
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
