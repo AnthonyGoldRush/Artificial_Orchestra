7c557b65e3249e6c422b7403aed629b7c951a812
AnthonyGoldRush <48116857+AnthonyGoldRush@users.noreply.github.com>
2021-08-26 21:51:04 -0700

    Update FaceMesh_Basics.py

--- FaceMesh_Basics.py
+++ FaceMesh_Basics.py
@@ -8,21 +8,26 @@ pTime = 0
 mpDraw = mp.solutions.drawing_utils
 mpFaceMesh = mp.solutions.face_mesh
 faceMesh = mpFaceMesh.FaceMesh(max_num_faces=2)
-drawSpec = mpDraw.DrawingSpec(thickness=1, circle_radius=2)
+drawSpec = mpDraw.DrawingSpec(thickness=2, circle_radius=2)
 
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = faceMesh.process(imgRGB)
     if results.multi_face_landmarks:
-        for faceLms in results.multi_face_ladmarks:
-            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACE_CONNECTIONS)
-
+        for faceLms in results.multi_face_landmarks:
+            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_CONTOURS,  # FACE_CONNECTIONS is updated to FACEMESH_CONTOURS
+            drawSpec,drawSpec)
+        for id,lm in enumerate(faceLms.landmark):
+            #print(lm)
+            ih, iw, ic = img.shape
+            x,y = int(lm.x*iw), int(lm.y*ih)
+            print(id,x,y)
 
     cTime = time.time()
-    fps = 1/(cTime-pTime)
+    fps = 1 / (cTime - pTime)
     pTime = cTime
-    cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3,
-                (0, 255, 0), 3)
+    cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN,
+    3, (255, 0, 0), 3)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
